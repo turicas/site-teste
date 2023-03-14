725e580a3e4caf0e2311a915aa9056c66f57e6e6
Álvaro Justen <alvarojusten@gmail.com>
2023-03-13 21:56:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Olá, mundo!"
+def index():
+  return "Olá, mundo! Esse é meu site. (Álvaro Justen)"
+
+@app.route("/sobre")
+def sobre():
+  return "Aqui vai o conteúdo da página Sobre"
+
+@app.route("/contato")
+def contato():
+  return "Aqui vai o conteúdo da página Contato"
