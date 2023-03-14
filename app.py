f48ca973073aa87457a2f040fc89dff327c1dace
Álvaro Justen <alvarojusten@gmail.com>
2023-03-13 22:09:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
+
 @app.route("/")
 def index():
-  return "Olá, mundo! Esse é meu site. (Álvaro Justen)"
+  return menu + "Olá, mundo! Esse é meu site. (Álvaro Justen)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da página Sobre"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da página Contato"
+  return menu + "Aqui vai o conteúdo da página Contato"
