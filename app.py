7c428d7e7130d46afd68f12f8c476301a0088155
Álvaro Justen <alvarojusten@gmail.com>
2023-03-13 22:27:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def ultimas_promocoes():
 
     
 menu = """
-<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
