802324b65e147fb27946fbea736541df6753c60e
Álvaro Justen <alvarojusten@gmail.com>
2023-03-20 20:06:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -45,3 +45,21 @@ def promocoes():
   for promocao in ultimas_promocoes():
     conteudo += f"<li>{promocao}</li>"
   return conteudo + "</ul>"
+
+
+@app.route("/promocoes2")
+def promocoes2():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  scraper = ChannelScraper()
+  contador = 0
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    conteudo += f"<li>{message.created_at} {texto}</li>"
+    if contador == 10:
+      break
+  return conteudo + "</ul>"
