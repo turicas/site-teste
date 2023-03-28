9e3abdee581a44de395490f3fad10288ad78e1a0
Álvaro Justen <alvarojusten@gmail.com>
2023-03-27 22:07:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def telegram_bot():
   chat_id = update["message"]["chat"]["id"]
   message = update["message"]["text"]
   nova_mensagem = {"chat_id": chat_id, "text": message}
-  requests.post(f"https://api.telegram.org./bot{token}/sendMessage", data=nova_mensagem)
+  requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
   return "ok"
 
 
