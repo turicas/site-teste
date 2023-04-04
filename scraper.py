72c3ce0156819621f0aca908f867416e1e25a7e3
Álvaro Justen <alvarojusten@gmail.com>
2023-04-03 22:10:56 -0300

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,13 @@
+from tchan import ChannelScraper
+
+
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
